eeae145b25e2362b013dcf3e9986d6405ef92ecb
MD. SAIFUL ISLAM <42407511+ocpsaifulislam@users.noreply.github.com>
2026-04-18 19:59:54 +0600

    Init

---
+++ src/main/java/dev/shoaibsuad/projectpush/BuildProjectPushApplication.java
@@ -0,0 +1,13 @@
+package dev.shoaibsuad.projectpush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildProjectPushApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildProjectPushApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/shoaibsuad/projectpush/BuildProjectPushApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.shoaibsuad.projectpush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildProjectPushApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
