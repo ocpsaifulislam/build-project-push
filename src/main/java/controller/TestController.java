b02088af244ada21c749a27f164a54be5d2b8759
MD. SAIFUL ISLAM <42407511+ocpsaifulislam@users.noreply.github.com>
2026-04-18 20:14:40 +0600

    Create Test Controller

---
+++ src/main/java/controller/TestController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v/api")
+public class TestController {
+    @GetMapping
+    public String test() {
+        return "Hello, World!";
+    }
+}
